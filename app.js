94bb11bf1902fe33d1264bf0d0e50b49f5493cab
blessed-tawanda <blessedmahuni@gmail.com>
2017-11-26 01:39:31 +0200

    added functionality to interface

--- app.js
+++ app.js
@@ -14,34 +14,37 @@ app.get('/', function(req,res) {
     res.sendFile(__dirname+"/index.html");
 });
 
-app.get('/download', function(req,res){
-    res.sendFile(__dirname+"/setup.exe");
+app.get('/upload', function(req,res){
+    res.sendFile(__dirname+'/upload.html');
+})
+
+app.get('/download',function(req,res){
+    res.sendFile(__dirname+'/download.html')
 })
 
 app.post('/upload',function(req,res){
     if(req.files){
-        //var nameOfFile;
+        var nameOfFile;
         var file = req.files.filename;
-        console.log(file)
-        // if(file.length==undefined){
-        //     nameOfFile = file.name;
-        //     file.mv("./Uploaded/"+nameOfFile,function(err){
-        //          if(err){
-        //              console.log(err);
-        //              res.send("An error occured")
-        //          }
-        //      })
-        // }
-        // for(var i = 0; i< file.length; i++)
-        // {
-        //    nameOfFile = file[i].name;
-        //    file[i].mv("./Uploaded/"+nameOfFile,function(err){
-        //         if(err){
-        //             console.log(err);
-        //             res.send("An error occured")
-        //         }
-        //     })
-        // }
-        // res.send("Done Upload Complete")
+        if(file.length==undefined){
+            nameOfFile = file.name;
+            file.mv("./Uploaded/"+nameOfFile,function(err){
+                 if(err){
+                     console.log(err);
+                     res.send("An error occured")
+                 }
+             })
+        }
+        for(var i = 0; i< file.length; i++)
+        {
+           nameOfFile = file[i].name;
+           file[i].mv("./Uploaded/"+nameOfFile,function(err){
+                if(err){
+                    console.log(err);
+                    res.send("An error occured")
+                }
+            })
+        }
+        res.send("Done Upload Complete")
     }
 })
